94ca557da96fd22db1312c537498fe42513c8abb
JanisSt <j.stals@gmail.com>
2020-10-20 14:33:13 +0300

    Chatroom

---
+++ Mono Chatroom/PINStorage.php
@@ -0,0 +1,66 @@
+<?php
+
+class PINStorage
+{
+    private string $path;
+    private array $pins = [];
+
+    public function __construct(string $path)
+    {
+
+        $this->path = $path;
+        $this->loadPINS();
+    }
+
+    public function searchPins()
+    {
+        if (isset($_POST['user'])) {
+            foreach ($this->pins as $pin) {
+                if ($pin->getUser() == $_POST['user'] &&
+                    $pin->getPIN() == $_POST['pin']
+                ) {
+                    return $pin;
+                }
+            }
+            return null;
+        }
+        return null;
+    }
+
+    public function loadPINS(): void
+    {
+        $file = fopen($this->path, 'rw+');
+        while ($pin = fgetcsv($file)) {
+            $this->pins[] = new PIN(
+                (string)$pin[0],
+                (string)$pin[1],
+
+            );
+        }
+        fclose($file);
+    }
+
+    public function checkPIN($zip): ?PIN
+    {
+        foreach ($this->pins as $pin) {
+            if ($pin->getPIN() == $zip) {
+                return $pin;
+            }
+        }
+        return null;
+    }
+
+
+    public function findPins(string $user): PIN
+    {
+        foreach ($this->pins as $pin) {
+            if ($pin->getUser() == $user
+            ) {
+                return $pin;
+            }
+
+
+        }
+    }
+
+}
